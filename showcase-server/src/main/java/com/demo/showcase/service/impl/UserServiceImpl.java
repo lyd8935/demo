2130f8865f13ad41f205d1a1adcbb58500bb8222
lyd8935 <lyd8935@126.com>
2016-10-27 19:45:56 +0800

    项目搭建

---
+++ showcase-server/src/main/java/com/demo/showcase/service/impl/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package com.demo.showcase.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.demo.showcase.commons.service.UserService;
+
+@Service("userService")
+public class UserServiceImpl implements UserService {
+	
+}
